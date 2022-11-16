ecf3e9216ae0d347272ed1a3b8b06904db34618b
John Haefele <jhaefele@gmail.com>
2022-11-15 21:41:00 -0600

    Cleaned up comments in db.js

--- services/db.js
+++ services/db.js
@@ -1,22 +1,4 @@
-// const mysql = require('mysql2/promise');
 const mariadb = require('mariadb')
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.DB_HOST,
-//     dialect: 'mariadb',
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// })
-
-// sequelize.authenticate().then(() => {
-//     console.log('Connection has been established successfully.');
-// }).catch(err => {
-//     console.error('Unable to connect to the database:', err);
-// });
 const config = require('../config');
 const pool = mariadb.createPool(config.db);
 
@@ -33,12 +15,3 @@ module.exports = {
         });
     }
 };
-
-// async function query(sql, params) {
-//     const [rows, fields] = await pool.execute(sql, params);
-//     return rows;
-// }
-
-// module.exports = {
-//   query
-// }
